e5b2a12fd8f60631d2db66b7c103ba1ff9275c1c
Abrar140 <muhamadabraramjad@gmail.com>
2025-02-26 10:19:33 +0500

    Done a problem

---
+++ 58-@-lengthofword.py
@@ -0,0 +1,34 @@
+# class Solution(object):
+#     def lengthOfLastWord(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+
+#         return len(s.split()[-1])
+
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = 0
+        i = len(s) - 1
+
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        print("i am here", i)
+
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
+
+s = "   fly me   to   the moon  "
+
+print(Solution().lengthOfLastWord(s))
--- Z-4-Median.py
+++ Z-4-Median.py
@@ -21,19 +21,49 @@ class Solution(object):
         :rtype: float
         """
         length=len(nums1)+len(nums2)
-        print(length,length//2)
-        nums1=nums1+nums2
+        i=0
+        j=0
+        
+        for z in range(length//2):
+            if i<len(nums1):
+                if nums1[i]>nums2[j]:
+                    j=j+1
+                else:
+                    i=i+1
+            if j<len(nums2):
+                if nums1[i]>nums2[j]:
+                    i=i+1
+                else:
+                    j=j+1
 
-        nums1.sort()
-        if len(nums1)%2==0:
-            return float((nums1[len(nums1)//2] + nums1[(len(nums1)//2)-1])/2)
+                print("i am abrar", nums1[i], nums2[j])
+        if (length%2==0):
+            print("i am here", nums1[i], nums2[j])
+            return (float(nums1[i]+nums2[j])/2)
         else:
-            return nums1[len(nums1)//2]
+            if len(nums1)>len(nums2):
+                return nums2[j]
+            else:
+                return nums1[i]
+            
+
+        #     print("i am here", nums1[i], nums2[j])
+        
+                
+
+        # nums1=nums1+nums2
+
+        # nums1.sort()
+        # print("i am sort", nums1)
+        # if len(nums1)%2==0:
+        #     return float((nums1[len(nums1)//2] + nums1[(len(nums1)//2)-1])/2)
+        # else:
+        #     return nums1[len(nums1)//2]
 
 
 
 # Example usage
-nums1 = [1,2,8,12]
-nums2 = [3,4]
+nums1 = [1]
+nums2 = [8,9]
 instance = Solution()
 print(instance.findMedianSortedArrays(nums1,nums2))  
