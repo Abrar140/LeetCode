1c052ff601850f4008db2482959f23b0a22ae9c2
Muhammad Abrar <muhamadabraramjad@gmail.com>
2025-09-20 18:40:36 +0500

    Done

--- z-1909-RemoveElementArrraySrictlyIncreasing.py
+++ z-1909-RemoveElementArrraySrictlyIncreasing.py
@@ -1,40 +1,64 @@
+# class Solution(object):
+#     def canBeIncreasing(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         if len(nums)==2:
+#             return True
+        
+     
+#         i=len(nums)-1
+#         n=len(nums)
+
+#         while i>0:
+#             print(i,"i")
+#             if i >= len(nums):  # <-- this avoids out of range
+#                i -= 1
+#                continue
+#             if nums[i-1]==nums[i] or nums[i-1]>nums[i]:
+#                 print(i,".....1",nums[i-1],nums[i])
+#                 if nums[i-2]==nums[i]:
+#                     print("i amhere")
+#                     nums.pop(i-2)
+#                 elif nums[i-2]>nums[i]:
+#                     nums.pop(i)
+#                 else:
+#                  nums.pop(i-1)
+#             else:
+#                 i=i-1
+#             print("i  amnums",nums)
+
+#         if len(nums)==n-1 or len(nums)==n:
+#             return True
+#         return False
+            
+
+
 class Solution(object):
     def canBeIncreasing(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        if len(nums)==2:
-            return True
-        
-     
-        i=len(nums)-1
-        n=len(nums)
+        count=0
+        for i in range(len(nums)):
+            pop(i)
+            if nums==sorted(nums) and count<2:
+                return True
+                count+=1
 
-        while i>0:
-            print(i,"i")
-            if i >= len(nums):  # <-- this avoids out of range
-               i -= 1
-               continue
-            if nums[i-1]==nums[i] or nums[i-1]>nums[i]:
-                print(i,".....1",nums[i-1],nums[i])
-                if nums[i-2]==nums[i]:
-                    print("i amhere")
-                    nums.pop(i-2)
-                elif nums[i-2]>nums[i]:
-                    nums.pop(i)
-                else:
-                 nums.pop(i-1)
-            else:
-                i=i-1
-            print("i  amnums",nums)
+            if nums[i]!=sortednums[i]:
+                count+=1
 
-        if len(nums)==n-1 or len(nums)==n:
+        if count<2:
             return True
-        return False
-            
+        else:
+            return False
+
+
         
 
-nums = [100,21,100]
+nums = [1,2,10,5,7]
 solution = Solution()
 print(solution.canBeIncreasing(nums))
