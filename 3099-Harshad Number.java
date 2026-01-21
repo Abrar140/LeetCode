e719817ef7d7fda21c160a36cb136ec88cb4835c
Muhammad Abrar <muhamadabraramjad@gmail.com>
2026-01-21 12:07:04 +0500

    21-Jan-2026

---
+++ 3099-Harshad Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+
+        int Sum=0;
+        int xs=x;
+        while(xs>0){
+            Sum=Sum+ (xs%10);
+            xs=xs/10;
+        }
+
+        if(x%Sum==0){
+            return Sum;
+        }
+        return -1;
+        
+    }
+}
